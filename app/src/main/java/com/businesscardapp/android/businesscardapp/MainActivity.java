3ca13a3f969c7ac7fd6ed3e199dcbd8e6a91d6f1
Himani Sharma <himanisharma2304@gmail.com>
2018-05-31 13:22:03 +0530

    --SingleScreenApp_Udacity

---
+++ app/src/main/java/com/businesscardapp/android/businesscardapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.businesscardapp.android.businesscardapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
